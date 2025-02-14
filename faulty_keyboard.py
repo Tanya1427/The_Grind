7b8bc5123f48e7e60d5ab75d3a3bf48f5d5d0005
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-02-13 23:16:44 -0600

    UMPIREd 2810. O(n) time solution. Used too much space though.

--- faulty_keyboard.py
+++ faulty_keyboard.py
@@ -1,45 +1,65 @@
 """
-Understand:
-DONT COPY/SLICE THE STRING
+Understand.
+mierogliphics
+ 0    1     2   3
+[m, erogl, ph, cs] -> n = len(_)
+201
+3102
 
-Match:
-String. Two Pointers
+if n is even, start with appendleft, else start with append
+if n is even, positions will start with even up to 0, else start with odds
+6 -> 6 4 2 0 1 3 5
+7 -> 7 5 3 1 0 2 4 6
 
-Plan:
-two pointers. When I pressed,
-reverse array from start up to i's location.
+7 5 3 1 0 2 4 6
 
-IMPLEMENT
+
+Match.
+Deque.
+
+Plan.
+form groups of splitted s with 'i'. n = len(groups)
+last = groups.pop()
+
+result = [""] * n
+index = 0
+incr = -2
+final_pos = n - 2
+Add the first indices to the result, reversed
+final_pos = abs(final_pos + 1)
+add the rest after resetting, without reversing
+
+```Might be tempting to use 1 while loop with a condition)
+   imo, twill use more operations for checking
+```
+
+Implement.
 """
-
 class Solution:
     def finalString(self, s: str) -> str:
-        typed = []
-        for ch in s:
-            if ch != 'i':
-                typed.append(ch)
-                continue
-            start, end = 0, len(typed)-1
-            while end > start:
-                typed[start], typed[end] = typed[end], typed[start]
-                start += 1
-                end -= 1
-
-        return "".join(typed)
+        groups = s.split('i')
+        n = len(groups)
+        final_pos = n - 2
+        index = 0
+        result = [0] * n
+        while final_pos >= 0:
+            result[index] = groups[final_pos][::-1]
+            index += 1
+            final_pos -= 2
+        final_pos = abs(final_pos + 1)  # Reset
+        while index < n:
+            result[index] = groups[final_pos]
+            index += 1
+            final_pos += 2
+        return "".join(result)
 
 """
-seneledan
-Reviewed
+Reviewed.
 
-abcdeighk
-  2
-abcde
-ebcda
-edcba
-edcba[]
+Evaluate:
+Space: O(n), for groups, for values from slices.
+Time: O(n).
 
-Evaluate.
-Space: O(n)
-Time: O(n**2)
-
+Better way, use a loop to gather letters,
+  switch between adding to front or back when 'i' is encountered.
 """
